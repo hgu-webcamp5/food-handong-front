bd8f6074be4153ea287496075008cc37c282927b
ohinhyuk <8156217@naver.com>
2023-01-05 03:34:46 +0900

    front main page
    outline 80% complete

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,39 @@
 import {TextFields} from "@mui/icons-material";
-import {Box, TextField , FormControl , InputLabel, Select , MenuItem} from "@mui/material";
+import {
+  Typography,
+  styled,
+  Paper,
+  Box,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  makeStyles
+} from "@mui/material";
 import {useState} from "react";
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import {createTheme , ThemeProvider} from "@mui/material/styles";
 
+const SideBar = styled(Paper)({
+    height: 800,
+    width: 220,
+    border: 1,
+    color: 'secondary.contratext',
+});
+const Item = styled(Paper)({
+  textAlign: 'center',
+  color: 'secondary.contratext',
+  height: 130,
+  lineHeight: '60px',
+});
+// const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 function Main() {
 
-  //
   const [age, setAge] = useState(0);
+  const [restaurants , setRestaurants] = useState([1,2,3,4,5,6,7,8]);
 
   const handleChange = (event) => {
     setAge(event.target.value);
@@ -15,38 +41,57 @@ function Main() {
 
   return(
   <div>
-  <div>
-    <Box>사이드 바</Box>
-    {/*<div>gg</div>*/}
-  </div>
-  <div>
-    <TextField id="outlined-basic" label="식당,음식,카테고리,지역 검색" variant="outlined" />
-  </div>
     <div>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label"> Age</InputLabel>
-        <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
-    <div>
-      <div>
-        <Box>맛집</Box><Box>맛집</Box>
-      </div>
+      <Grid container spacing={5} sx={{ ml:-18 , width : '120%' }}>
+        <Grid item xs={2} >
+              <SideBar elevation={5} >side bar</SideBar>
+        </Grid>
+        <Grid item xs={10}>
+            <Box
+                sx={{
+                    p: 2,
+                    display: 'grid',
+                    gridTemplateColumns: { md: '1fr 1fr' },
+                    gap: 80,
+                }}
+            >
+            <TextField id="outlined-basic" label="식당,음식,카테고리,지역 검색" variant="outlined" />
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label"> Age</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            </Box>
 
-      <div>
-        <Box>맛집</Box><Box>맛집</Box>
-      </div>
-    </div>
+            <Box
+                sx={{
+                  p: 3,
+                  display: 'grid',
+                  gridTemplateColumns: { md: '1fr 1fr' },
+                  gap: 5,
+                }}
+            >
+              {restaurants.map((index) => (
+                  <Item key={index} elevation={5}>
+                    {`음식점${index}`}
+                  </Item>
+              ))}
+            </Box>
+        </Grid>
+
+      </Grid>
+  </div>
+
+
   </div>
 
   );
